cc5d5874628933615fbcb45c5a7fbd72a1bf6ece
leon <leon@yun9.com>
2015-10-20 18:54:37 +0800

    1

--- security/test/filter.all.js
+++ security/test/filter.all.js
@@ -6,15 +6,15 @@ var express = require("express")
     , request = require("supertest")
     , security = require("../")
     , filter = security.Filter()
-    , store = security.FilterStore();
+    , store = security.FilterStore()
+    , app = express()
+    , router = express.Router();
 
-describe("filter.all", function (done) {
-    it("request", function () {
-        var app = express();
-        var router = express.Router();
 
+describe("filter.all", function () {
+    beforeEach(function () {
         //注册过滤器仓库
-        store.use("/hello", {}).use("/admin*").use("/app*");
+        store.use("/hello", {}).use("/admin*").use("/app*").use("/demo1/hello");
         filter.store(store);
 
         //注册过滤策略
@@ -22,27 +22,46 @@ describe("filter.all", function (done) {
 
         //注册中间件
         filter(app, {
-            demo: "demo1"
+            root: "/demo1"
+        });
+
+        //注册业务路由
+        router.get("/app/order", function (req, res) {
+            res.send({
+                orderid: "123457767867"
+            })
         });
 
         router.get("/hello", function (req, res) {
+            res.send({
+                name: "tobi"
+            });
+        });
 
-            req.local = {
-                title: "aadfadf"
-            };
-
-            res.send(200, {
+        router.get("/demo1/hello", function (req, res) {
+            res.send({
                 name: "tobi"
             });
         });
 
         app.use("/", router);
+    });
 
+    it("request", function (done) {
         request(app)
-            .get("/hello")
-            .expect(302)
-            .end(function () {
-                done();
+            .get("/demo1/hello")
+            .expect(200)
+            .end(function (error, res) {
+                done(error);
             });
     });
+
+    it("request order", function (done) {
+        request(app)
+            .get("/app/order")
+            .expect(200)
+            .end(function (error, res) {
+                done(error);
+            });
+    })
 })
