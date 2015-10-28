34df2314f99f2f391f7e2210e6129e44ca177c36
CKHuang <ckhuang326@icloud.com>
2015-10-28 09:14:08 +0800

    修复bug

--- repository/lib/Repository.js
+++ repository/lib/Repository.js
@@ -12,16 +12,20 @@
  */
 var proto = module.exports = function (options) {
 
-    var repository = function (res, req, next) {
-        //repository
-        res.y9Repository = repository;
-        next();
+    var repository = function () {
+        return function (res, req, next) {
+            //repository
+            res.y9Repository = repository;
+            next();
+        }
     }
 
     repository.__proto__ = proto;
 
     repository._models = [];
     repository._options = options || {};
+    repository._modelNameField = repository._options.modelNameField || 'y9ModelName';
+    repository._paramsField = repository._options.paramsField || 'y9Params';
     repository.init();
 
     return repository;
@@ -29,6 +33,49 @@ var proto = module.exports = function (options) {
 
 proto.init = function init() {
 
+}
+
+/**
+ * 中间件，处理model请求
+ * 根据model名称以及参数，返回具体资源
+ * @returns {Function}
+ */
+proto.service = function() {
+    var self = this;
+
+    return function(req, res, next) {
+        //从请求获取参数
+        var modelName = req.body[self._modelNameField];
+        var params = req.body[self._paramsField];
+
+        var errResData = {
+            code: "500",
+        }
+
+        if (!modelName) {
+            errResData.cause = "请求参数中无法找到名称为:" + self._modelNameField+ "的模块名称参数."
+            res.statusCode = 500;
+            res.send(errResData);
+            return;
+        }
+
+        var model = req.y9Repository.get(modelName);
+        if (!model) {
+            errResData.cause = "无法获取名称为："+modelName+" 的模块配置";
+            res.statusCode = 500;
+            res.send(errResData);
+            return;
+        }
+        model.action(rq.y9proxy,params,function(err,res) {
+            if (err) {
+                errResData.cause = err.message;
+                res.statusCode = 500;
+                res.send(errResData);
+            } else {
+                res.send(res.toJSON());
+            }
+        });
+    }
 }
 
 /***
